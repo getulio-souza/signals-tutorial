f1a43030dd925d03f7144be6751da5ee842f1be1
getulio.souza <getulio.souza@evolutix.com.br>
2026-01-04 20:32:58 -0300

    feature: effect

--- src/app/computed-signals/computed-signals.component.ts
+++ src/app/computed-signals/computed-signals.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, OnInit, Signal, signal, WritableSignal } from '@angular/core';
+import { Component, computed, OnInit, Signal, signal, WritableSignal, effect } from '@angular/core';
 
 @Component({
   selector: 'app-computed-signals',
@@ -6,8 +6,43 @@ import { Component, computed, OnInit, Signal, signal, WritableSignal } from '@an
   styleUrls: ['./computed-signals.component.scss']
 })
 export class ComputedSignalsComponent implements OnInit {
+
+  constructor() {
+    //using effect
+    effect(() => {
+      console.log(`the count after effect is: ${this.count()}`)
+    })
+
+  }
+
   count: WritableSignal<number> = signal(0)
   doubleCount: Signal<number> = computed(() => this.count() * 2)
+
+  items: WritableSignal<number[]> = signal([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+
+  showCount = signal(false)
+
+  conditionalCount = computed(() => {
+    if (!this.showCount()) {
+      console.log('nothing to show here')
+      return `nothing to show here`
+    } else {
+      return `the count value is ${this.count()}`
+    }
+  })
+
+  updateShowCount() {
+    this.showCount.update((value)=> !value)
+  }
+
+  getContionalCount() {
+    console.log(this.conditionalCount())
+  }
+
+  dividerByTwo: Signal<number[]> = computed(() => {
+    console.log('filtering event numbers...')
+    return this.items().filter((item)=> item % 3 === 0)
+  })
 
   expensiveComputation = computed(() => {
     console.log('expensive computation running...')
@@ -20,12 +55,17 @@ export class ComputedSignalsComponent implements OnInit {
     console.log(this.doubleCount())
   }
 
+  doubleNumbers() {
+    this.dividerByTwo()
+    console.log(this.dividerByTwo())
+  }
+
   expensiveCount() {
     console.log(this.expensiveComputation())
   }
 
   increment() {
-    this.count.update((value) => value + 2)
+    this.count.update((value) => value + 1)
     console.log(this.count())
   }
 }
