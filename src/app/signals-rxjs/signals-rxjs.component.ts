bfbd31ae6e5d7b23509501623f92ea8ba5876744
getulio.souza <getulio.souza@evolutix.com.br>
2026-01-10 10:38:53 -0300

    feature: signals-rxjs

--- src/app/signals-rxjs/signals-rxjs.component.ts
+++ src/app/signals-rxjs/signals-rxjs.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import {toSignal} from '@angular/core/rxjs-interop'
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-signals-rxjs',
   templateUrl: './signals-rxjs.component.html',
-  styleUrls: ['./signals-rxjs.component.scss']
+  styleUrls: ['./signals-rxjs.component.scss'],
 })
-export class SignalsRxjsComponent {
-  counterObservable = interval(1000);
+export class SignalsRxjsComponent implements OnInit, OnDestroy{
+  counterObservable = interval(10000);
+  private subscription!: Subscription
 
-  counter = toSignal(this.counterObservable, {initialValue: 0})
+  counter = toSignal(this.counterObservable, { initialValue: 0 }, { manualCleanup: true });
+
+  ngOnInit(): void {
+    this.subscription = this.counterObservable.subscribe()
+  }
+
+  ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe()
+    }
+  }
 }
